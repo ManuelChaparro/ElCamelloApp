353357b0b9ebf71f030b4f84302eab7f332998b0
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-03-20 21:41:31 -0500

    Avances en el token

--- FrontEnd Camello/src/app/home/home.component.ts
+++ FrontEnd Camello/src/app/home/home.component.ts
@@ -7,22 +7,45 @@ import jwt_decode from 'jwt-decode';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  public name: string;
+  public name: string | unknown;
   public surname: string;
   public number: number | undefined;
+  public rol: string | unknown;
 
   constructor(){
-    const token = localStorage.getItem('token');
-    const decode_token: string | undefined = jwt_decode(JSON.stringify(token));
-    console.log(typeof(decode_token));
-
-
     this.name = '';
     this.surname = '';
     this.number = undefined;
+    this.rol = '';
+
+    const token = localStorage.getItem('token');
+    const decode_token: object = jwt_decode(JSON.stringify(token));
+
+    if('exp' in decode_token){
+      const exp = decode_token.exp as number;
+      const date = new Date(0);
+      date.setUTCSeconds(exp);
+      console.log(date.toISOString());
+
+      const format_date = date.setUTCSeconds(exp)
+
+
+      const now = new Date();
+
+      console.log(now); // 👉️ 2023-01-20T12:01:02.900Z
+
+    }
+
+
+    if('infoUser' in decode_token){
+      const xd = decode_token.infoUser as Array<object>;
+      if('rol' in xd[0] && 'email' in xd[0]){
+        this.name = xd[0].email;
+        this.rol = xd[0].rol;
+      }
+
+
+    }
   }
-
-
-
 
 }
