572c12bc05142728e17dcb053b93922e59a3fbfa
Luis Sandoval <luchoparrrasandoval@gmail.com>
2023-03-20 20:47:52 -0500

    Se modificó prueba unitaria del modulo de usuarios

--- BackEnd Camello/src/tests/users.loginUser.test.js
+++ BackEnd Camello/src/tests/users.loginUser.test.js
@@ -1,69 +1,87 @@
 'use strict'
 
-const request = require('supertest');
-const app = require('../app');
-const connection = require('../../config/connections');
-const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const crypto = require("crypto");
+const connection = require('../../config/connections.js');
+const loginUser = require('./loginUser.js'); // import the function to be tested
 
-describe('Prueba de login', () => {
-    // Antes de cada prueba, crea un usuario en la base de datos
-    beforeEach(async done => {
-        const email = 'test@example.com';
-        const password = 'password123';
-        const email_binary = crypto.createHash('sha256').update(email).digest('hex');
-        const password_binary = crypto.createHash('sha256').update(password).digest('hex');
-        const query = `INSERT INTO usuarios (email) VALUES (${connection.escape(email)}); INSERT INTO passwords (indicador, password) VALUES (${connection.escape(email_binary)}, ${connection.escape(password_binary)})`;
-        await connection.query(query, (err, results) => {
-            if (err) throw err;
-            done();
-        });
-    });
-
-    // Despues de cada prueba, elimina el usuario que creó
-    afterEach(async done => {
-        const email = 'test@example.com';
-        const email_binary = crypto.createHash('sha256').update(email).digest('hex');
-        const query = `DELETE FROM usuarios WHERE email = ${connection.escape(email)}; DELETE FROM passwords WHERE indicador = ${connection.escape(email_binary)}`;
-        await connection.query(query, (err, results) => {
-            if (err) throw err;
-            done();
-        });
-    });
-
-    //Probando un inicio de sesión exitoso
-    test('POST /login - success', async () => {
-        const email = 'test@example.com';
-        const password = 'password123';
-        const response = await request(app).post('/login').send({email, password}).expect(200);
-        expect(response.body.token).toBeDefined();
-    });
-
-    // Probando una contraseña incorrecta
-    test('POST /login - incorrect password', async () => {
-        const email = 'test@example.com';
-        const password = 'wrongpassword';
-        const response = await request(app).post('/login').send({email, password}).expect(200);
-        expect(response.body.message).toBe('Contraseña incorrecta');
-    });
-
-    // Probando un correo incorrecto
-    test('POST /login - incorrect email', async () => {
-        const email = 'wrongemail@example.com';
-        const password = 'password123';
-        const response = await request(app).post('/login').send({email, password}).expect(200);
-        expect(response.body.message).toBe('Email incorrecto');
-    });
-});
+jest.mock(connection, () => ({
+    query: jest.fn(),
+    escape: jest.fn((val) => `"${val}"`)
+}));
 
 describe('loginUser', () => {
-    test('Debe devolver un token cuando el correo electrónico y la contraseña son correctos', async () => {
-        const req = { body: { email: 'test@example.com', password: 'password' } };
-        const res = {
-            json: jest.fn().mockImplementation((response) => response),
+    let req, res;
+
+    beforeEach(() => {
+        req = {
+            body: {
+                email: 'test@example.com',
+                password: 'password'
+            }
         };
+        res = {
+            json: jest.fn()
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should return a token if email and password are correct', async () => {
+        const mockResult = [{ password: crypto.createHash('sha256').update('password').digest('hex') }];
+        const mockInfoUser = { id: 1, email: 'test@example.com' };
+        const mockToken = 'mock-token';
+
+        // mock the database query results
+        connection.query.mockImplementation((query, callback) => {
+            if (query.includes('passwords')) {
+                callback(null, mockResult);
+            } else {
+                callback(null, [mockInfoUser]);
+            }
+        });
+
+        // mock the JWT sign method
+        jwt.sign = jest.fn().mockImplementationOnce((payload, secretOrPrivateKey, options, callback) => {
+            callback(null, mockToken);
+        });
+
         await loginUser(req, res);
-        expect(res.json).toHaveBeenCalled();
-        expect(res.json.mock.calls[0][0].token).toBeDefined();
+
+        expect(connection.query).toHaveBeenCalledTimes(2);
+        expect(jwt.sign).toHaveBeenCalledTimes(1);
+        expect(res.json).toHaveBeenCalledWith({ token: mockToken });
+    });
+
+    it('should return an error message if the email is incorrect', async () => {
+        const mockResult = [];
+        const expectedMessage = 'Email incorrecto';
+
+        // mock the database query results
+        connection.query.mockImplementation((query, callback) => {
+        callback(null, mockResult);
+        });
+
+        await loginUser(req, res);
+
+        expect(connection.query).toHaveBeenCalledTimes(1);
+        expect(res.json).toHaveBeenCalledWith({ message: expectedMessage });
+    });
+
+    it('should return an error message if the password is incorrect', async () => {
+        const mockResult = [{ password: 'wrong-password' }];
+        const expectedMessage = 'Contraseña incorrecta';
+
+        // mock the database query results
+        connection.query.mockImplementation((query, callback) => {
+        callback(null, mockResult);
+        });
+
+        await loginUser(req, res);
+
+        expect(connection.query).toHaveBeenCalledTimes(1);
+        expect(res.json).toHaveBeenCalledWith({ message: expectedMessage });
     });
 });
