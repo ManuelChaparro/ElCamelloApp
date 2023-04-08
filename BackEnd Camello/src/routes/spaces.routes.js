dd253207c6fa47a8b9229202c9b7367c1bddbcf9
Luis Sandoval <luchoparrrasandoval@gmail.com>
2023-04-07 23:34:51 -0500

    Se agregaron los endpoint para las funciones de espacios

--- BackEnd Camello/src/routes/spaces.routes.js
+++ BackEnd Camello/src/routes/spaces.routes.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const router = require('express').Router()
-const {} = require('../controllers/spaces.controller')
+const { createSpace, modifySpace, deleteSpace, getSpacesList, searchSpace } = require('../controllers/spaces.controller')
 const {verifyToken} = require('../controllers/users.controller')
 
-
+router.post('/spaces/add', verifyToken, createSpace)
+router.fetch('/spaces/modify', verifyToken, modifySpace)
+router.delete('/spaces/delete', verifyToken, deleteSpace)
+router.get('/spaces/list', verifyToken, getSpacesList)
+router.get('/spaces/search', verifyToken, searchSpace)
 
 module.exports = router
