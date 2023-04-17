569dab23fae7b9a5559e6b78ce3e8bcc9fb000cd
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-04-17 12:26:20 -0500

    Avances en eliminacion de sede

--- FrontEnd Camello/src/app/campuslist/campuslist.component.ts
+++ FrontEnd Camello/src/app/campuslist/campuslist.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 
+interface campus{
+  name: string,
+  description: string,
+  address: string,
+  id_sede: number
+}
+
 @Component({
   selector: 'app-campuslist',
   templateUrl: './campuslist.component.html',
   styleUrls: ['./campuslist.component.css']
 })
+
 export class CampuslistComponent {
+
+  public campus_list: Array<campus>;
 
   ngOnInit(){
     this.loadCampusOnDatabase();
   }
 
   constructor(private http: HttpClient){
-    
+    this.campus_list = [];
   }
 
   loadCampusOnDatabase(){
@@ -21,13 +31,27 @@ export class CampuslistComponent {
     const headers = new HttpHeaders({
       Authorization: 'Bearer ' + localStorage.getItem('token'),
     });
-    console.log(1);
 
     this.http.get(url, {headers}).subscribe(response => {
-      console.log(1);
-      console.log(response);
-
+      const campus = response as Array<string>;
+      campus.forEach(n => {
+        const info_campus = n as Object;
+        if('nombre_sede' in info_campus && 'descripcion' in info_campus && 'direccion' in info_campus
+        && 'id_sedes' in info_campus){
+          const campus_obj = {
+            name: info_campus.nombre_sede as string,
+            description: info_campus.descripcion as string,
+            address: info_campus.direccion as string,
+            id_sede: info_campus.id_sedes as number,
+          }
+          this.campus_list.push(campus_obj as campus);
+        }
+      });
     });
+  }
+
+  deleteCampus(id_sedes: number){
+
   }
 
 }
