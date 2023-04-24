5dad566261207c04fbc377535edb9f380da87378
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-04-24 02:08:56 -0500

    Añadir producto finalizado

--- FrontEnd Camello/src/app/stocklist/stocklist.component.ts
+++ FrontEnd Camello/src/app/stocklist/stocklist.component.ts
@@ -8,6 +8,7 @@ interface Stock{
 }
 
 interface Product{
+  id: number,
   name: string,
   brand: string,
   type: string,
@@ -20,14 +21,16 @@ interface Product{
   styleUrls: ['./stocklist.component.css']
 })
 export class StocklistComponent {
-  public stockList: Array<Stock> | undefined;
+  public stockList: Array<Stock>;
+  private idProductDelete: number;
 
   ngOnInit(){
     this.loadStockList();
   }
 
   constructor(private http: HttpClient){
-    this.stockList = undefined;
+    this.stockList = [];
+    this.idProductDelete = 0;
   }
 
   private loadStockList(): void{
@@ -51,10 +54,36 @@ export class StocklistComponent {
   }
 
   private addListProducts(stock: Array<any>, rol: string): void{
-    stock.forEach(stock => {
+      stock.forEach(stock => {
       const newStock: Stock = {name: stock.nombre_sede, listProducts: []};
-      
+      const url = 'http://localhost:3005/api/inventary/product/filter';
+      const data = {
+        inventary_id: stock.id_inventario,
+        rol: rol,
+      };
+      const headers = new HttpHeaders({
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      });
+      this.http.post(url, data, {headers}).subscribe(response => {
+        const products = response as Array<any>;
+        console.log(products);
+
+        products.forEach(product => {
+          const newProduct: Product = {
+            id: product.id_producto,
+            name: product.nombre_producto,
+            brand: product.marca,
+            type: product.tipo_producto,
+            price: product.valor_producto};
+          newStock.listProducts.push(newProduct);
+        });
+        this.stockList?.push(newStock);
+      });
     });
+
+  }
+
+  public confirmDeleteProduct(id_product: string): void{
 
   }
 }
