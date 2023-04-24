d3609c3a866ddcfd525f77dbeaf8ce19036e680b
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-04-23 23:07:29 -0500

    Avances en front de productos

--- FrontEnd Camello/src/app/newstock/newstock.component.ts
+++ FrontEnd Camello/src/app/newstock/newstock.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as bootstrap from 'bootstrap';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 interface stockObject{
   id: number,
@@ -16,20 +17,30 @@ interface stockObject{
 })
 export class NewstockComponent {
 
+  public campus_list: Iterable<any> | null | undefined;
   public countObjs: number;
   public stockName: string;
   public stockType: string;
   public stockDescription: string;
+  public stockBrand: string;
   public stockPrice: number | undefined;
   public listStock: Array<stockObject>;
+  public campus_selected: string | null | undefined;
 
-  constructor(){
+  constructor(private http: HttpClient){
     this.countObjs = 1;
     this.stockName = '';
     this.stockType = 'Mueble';
     this.stockDescription = '';
     this.stockPrice = undefined;
     this.listStock = [];
+    this.stockBrand = '';
+    this.campus_selected = undefined;
+    this.campus_list = undefined;
+  }
+
+  private ngOnInit(){
+    this.initCampusList();
   }
 
   addObjToStock(){
@@ -51,6 +62,21 @@ export class NewstockComponent {
     name.value = '';
     description.value = '';
     price.value = '';
+  }
+
+  initCampusList(){
+    const url = 'http://localhost:3005/api/headquarters/list';
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('token'),
+    });
+    this.http.get(url, { headers }).subscribe((data) => {
+      this.campus_list = data as Iterable<any>;
+      const campus_array = Array.from(this.campus_list);
+      const ultimoElemento = campus_array.pop();
+      if('nombre_sede' in ultimoElemento){
+        this.campus_selected = ultimoElemento.nombre_sede;
+      }
+    });
   }
 
   validations(): boolean{
@@ -85,6 +111,7 @@ export class NewstockComponent {
     const labelName = document.querySelector('#warn-name') as HTMLElement;
     const labelDescription = document.querySelector('#warn-description') as HTMLElement;
     const labelPrice = document.querySelector('#warn-price') as HTMLElement;
+    const labelBrand = document.querySelector('#warn-brand') as HTMLElement;
     if(value === 0){
       if(this.stockName != ''){
         labelName.style.display = 'none';
@@ -93,7 +120,11 @@ export class NewstockComponent {
       if(this.stockDescription != ''){
         labelDescription.style.display = 'none';
       }
-    }else{
+    }else if(value === 3){
+      if(this.stockBrand != ''){
+        labelBrand.style.display = 'none';
+      }
+    }else if(value === 2){
       if(this.stockPrice != 0){
         labelPrice.style.display = 'none';
       }
@@ -107,9 +138,4 @@ export class NewstockComponent {
   changeStockType(value:string): void{
     this.stockType = value;
   }
-
-
-
-
-
 }
