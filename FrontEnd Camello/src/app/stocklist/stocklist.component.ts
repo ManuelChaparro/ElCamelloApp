c296f9a0727967983bc00a4495ec4d8c05beccad
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-04-24 01:48:52 -0500

    Avances en front de productos 5

--- FrontEnd Camello/src/app/stocklist/stocklist.component.ts
+++ FrontEnd Camello/src/app/stocklist/stocklist.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
+import jwt_decode from 'jwt-decode';
 
 interface Stock{
-  description: string,
+  name: string,
   listProducts: Product[]
 }
 
@@ -19,9 +20,9 @@ interface Product{
   styleUrls: ['./stocklist.component.css']
 })
 export class StocklistComponent {
-  public stockList: Iterable<Stock> | undefined;
+  public stockList: Array<Stock> | undefined;
 
-  private ngOnInit(){
+  ngOnInit(){
     this.loadStockList();
   }
 
@@ -30,8 +31,30 @@ export class StocklistComponent {
   }
 
   private loadStockList(): void{
-    
+    const decode_token: object = jwt_decode(JSON.stringify(localStorage.getItem('token')));
+    if('infoUser' in decode_token){
+      const infoUser =  decode_token.infoUser as Array<object>;
+      if('rol' in infoUser[0]){
+        const url = 'http://localhost:3005/api/inventary/list';
+        const rol = infoUser[0].rol as string
+        const data = {
+          rol: rol,
+        };
+        const headers = new HttpHeaders({
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        });
+        this.http.post(url, data, {headers}).subscribe(response => {
+          this.addListProducts(response as Array<any>, rol);
+        });
+      }
+    }
   }
 
+  private addListProducts(stock: Array<any>, rol: string): void{
+    stock.forEach(stock => {
+      const newStock: Stock = {name: stock.nombre_sede, listProducts: []};
+      
+    });
 
+  }
 }
